8f8c4ea3a2f14dfe8f5b401270cc825e54bdfc90
Alex Headley <aheadley@waysaboutstuff.com>
2010-11-15 01:15:44 -0500

    need to fix post.thread name clash

--- dibs/ib/models.py
+++ dibs/ib/models.py
@@ -3,16 +3,16 @@ from django.db import models
 # Create your models here.
 
 class Thread(models.Model):
-    image_src = models.ImageField(max_length=255)
-    thumb_src = models.ImageField(max_length=255)
+    image_src = models.ImageField(max_length=255, upload_to='src/')
+    thumb_src = models.ImageField(max_length=255, upload_to='thumbs/')
     is_spoiler = models.BooleanField()
     timestamp = models.DateTimeField(auto_now_add=True)
-    poster_name = models.CharField()
-    poster_ip = IPAddressField()
-    poster_email = models.CharField()
-    subject = models.CharField()
+    poster_name = models.CharField(max_length=64)
+    poster_ip = models.IPAddressField()
+    poster_email = models.CharField(max_length=64)
+    subject = models.CharField(max_length=128)
     content = models.TextField()
-    password = models.CharField()
+    password = models.CharField(max_length=64)
 
 class Post(Thread):
     thread = models.ForeignKey(Thread)
