98ca414630ab9a919c8f761f83ed31e2863c0ba2
Alex Headley <aheadley@waysaboutstuff.com>
2010-11-15 00:53:24 -0500

    added some ib/models, added ib to installed apps

--- dibs/ib/models.py
+++ dibs/ib/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Thread(models.Model):
+    image_src = models.ImageField(max_length=255)
+    thumb_src = models.ImageField(max_length=255)
+    is_spoiler = models.BooleanField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    poster_name = models.CharField()
+    poster_ip = IPAddressField()
+    poster_email = models.CharField()
+    subject = models.CharField()
+    content = models.TextField()
+    password = models.CharField()
+
+class Post(Thread):
+    thread = models.ForeignKey(Thread)
