3f617d13f40645870cf74414c84bb30f844a39e2
aheadley <aheadley@waysaboutstuff.com>
2010-11-29 18:45:31 -0500

    this should probably be an app instead of a project

--- urls.py
+++ urls.py
@@ -1,10 +1,16 @@
 from django.conf.urls.defaults import *
-
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+from views import *
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    (r'^admin/', include(admin.site.urls)),
-	(r'^', include('ib.urls')),
+urlpatterns = patterns('ib.views',
+    url(r'^(?:index/?)?$',
+        index,
+        name='index_view'),
+    url(r'^(?P<board_slug>[A-Za-z0-9_-]+)/?$',
+        board,
+        name='board_view'),
+    url(r'^(?P<board_slug>[A-Za-z0-9_-]+)/(?P<thread_id>\d+)/?$',
+        thread,
+        name='thread_view'),
 )
